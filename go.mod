36768cadda91d919157cf4883d349b1a9f3065a7
FelipeSoft <felipedecastrolima2@gmail.com>
2025-06-09 15:05:10 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FelipeSoft/golang-api-with-ci-cd
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+);
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
+	defer cancel()
+
+	mux := http.NewServeMux();
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request, ) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	go func() {
+		log.Printf("HTTP server listening on 127.0.0.1:8001")
+		if err := http.ListenAndServe("127.0.0.1:8001", mux); err != nil {
+			log.Fatalf("Could not start the HTTP server on 127.0.0.1:8001: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+}
